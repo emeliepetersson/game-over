304db8c4f02be4159eb560411c3c1fd65bf5897f
Cleanly <oskar.turesson99@gmail.com>
2020-05-18 20:35:12 +0200

    removed unnecessary editorConfig lines

--- Public/src/scripts/Timer.js
+++ Public/src/scripts/Timer.js
@@ -1,55 +1,55 @@
 function counter(maxTime) {
-    var i = maxTime;
-    time = maxTime;
-    seconds = setInterval(
-        function () {
-            i--;
-            time = i;
-        },
-        1000,
-        i
-    );
+  var i = maxTime;
+  time = maxTime;
+  seconds = setInterval(
+    function () {
+      i--;
+      time = i;
+    },
+    1000,
+    i
+  );
 }
 
 class Timer {
-    score = 0;
-    scorePerSecond;
-    end = false;
+  score = 0;
+  scorePerSecond;
+  end = false;
 
-    constructor(maxTime, scorePerSecond) {
-        this.time = maxTime;
-        this.maxTime = maxTime;
-        this.scorePerSecond = scorePerSecond;
-    }
+  constructor(maxTime, scorePerSecond) {
+    this.time = maxTime;
+    this.maxTime = maxTime;
+    this.scorePerSecond = scorePerSecond;
+  }
 
-    draw() {
-        this.time = time;
-        if (this.time === 0) {
-            gameOver();
-        }
-        push();
-        fill(0, 0, 0);
-        stroke(51);
-        textSize(20);
-        text(`Time Left: ${this.time}`, 5, 20);
-        text(`Score: ${this.score}`, 5, 40);
-        text(`High Score: ${this.highScore}`, 5, 60);
-        pop();
+  draw() {
+    this.time = time;
+    if (this.time === 0) {
+      gameOver();
     }
+    push();
+    fill(0, 0, 0);
+    stroke(51);
+    textSize(20);
+    text(`Time Left: ${this.time}`, 5, 20);
+    text(`Score: ${this.score}`, 5, 40);
+    text(`High Score: ${this.highScore}`, 5, 60);
+    pop();
+  }
 
-    start() {
-        counter(this.maxTime);
-    }
+  start() {
+    counter(this.maxTime);
+  }
 
-    end() {
-        clearInterval(seconds);
-        const score = this.time * this.scorePerSecond;
-        gameStart = false;
-        return score;
-    }
+  end() {
+    clearInterval(seconds);
+    const score = this.time * this.scorePerSecond;
+    gameStart = false;
+    return score;
+  }
 
-    gameOver() {
-        clearInterval(seconds);
-        gameStart = false;
-    }
+  gameOver() {
+    clearInterval(seconds);
+    gameStart = false;
+  }
 }
